b8e81610018d872677ea5e36817b6ff792c1b800
Mosab Sasi <smosab@gmail.com>
2016-01-04 20:32:04 -0700

    Rest of Medium 3 questions, Hard 1 questions 1-2

---
+++ Lesson_3/Hard_1.rb
@@ -0,0 +1,10 @@
+Answer 1
+
+Local variabe undefined error will be raised
+
+Answer 2
+
+puts greetings # => 'hi'
+
+Answer 3
+
--- Lesson_3/Medium_3.rb
+++ Lesson_3/Medium_3.rb
@@ -1,10 +1,27 @@
 
 Answer 3
 
-require 'pry'
+My string looks like this now: pumpkins
+#Ruby is reassigining the value"rutabaga" to a new object withing the method with the '+=' operator.
 
-puts "My string looks like this now: pumpkins" # Ruby is reassigining the value"rutabaga" to a new object withing the method with the '+=' operator.
-puts "My array looks like this now: ["pumpkins", "rutabaga"]" #Ruby is using the same object.
+My array looks like this now: ["pumpkins", "rutabaga"]
+#Ruby is using the same object.
 
 
+Answer 4
 
+My string looks like this now: rutubaga
+My array looks like this now: ["pumpkins"]
+
+Answer 5
+
+def color_valid(color)
+  case color
+  when "blue"
+    true
+  when "green"
+    true
+  else
+    false
+  end
+end
