90dc031a4cc6531ba4eeb412f85a540e9cc6f0f9
Mosab Sasi <smosab@gmail.com>
2016-01-06 21:47:35 -0700

    Hard 1, exercises 1-4

--- Lesson_3/hard_1_question_4.rb
+++ Lesson_3/hard_1_question_4.rb
@@ -1,43 +1,3 @@
-Answer 1
-
-Local variabe undefined error will be raised
-
-Answer 2
-
-puts greetings # => 'hi'
-
-Answer 3
-
-A)
-#=> One is: one
-#=> two is: two
-#=> three is: three
-
-B)
-#=> One is: one
-#=> two is: two
-#=> three is: three
-
-C)
-#=> One is: two
-#=> two is: three
-#=> three is: one
-
-Answer 4
-
-# Easy way:
-
-require 'SecureRandom'
-
-def uuid
-  SecureRandom.uuid
-end
-
-uuid
-=> "6ec3d795-355a-4d26-a542-34170028d258"
-
-# Hard way:
-
 
 uuid = []
 
@@ -120,5 +80,3 @@ uuid << fifth_hex
 uuid = uuid.join('-')
 
 puts uuid
-
-Answer 5
